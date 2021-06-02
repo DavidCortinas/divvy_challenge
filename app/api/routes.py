69e77b4874c1ccc832fe051e9887d6ccd04d602d
davidcortinas <dcortinas33@gmail.com>
2021-06-02 17:38:39 -0500

    added instructions

--- app/api/routes.py
+++ app/api/routes.py
@@ -3,13 +3,24 @@ from flask import jsonify
 from app.api.models import Trip
 from app.api import db
 from flask.globals import request
+from datetime import datetime
 
 @app.route('/', methods=['GET'])
-def calculate():
+def instructions():
     # data = {
     #     "This": "Works"
     # }
 
-    data = "Navigate to '.../calculate' and add the following query parameters: start (i.e. '2013-06-28'), end (i.e. '2013-06-30)"
+    data = "Navigate to '.../calculate' and add the following query parameters: start (i.e. '2013-06-28'), end (i.e. '2013-06-30')"
+
+    return data
+
+@app.route('/calculate?start=<starttime>&&end=<stoptime>', methods=['GET'])
+def calculate(starttime, stoptime):
+
+
+    data = {
+        "averageDuration": stoptime-starttime
+    }
 
     return data
